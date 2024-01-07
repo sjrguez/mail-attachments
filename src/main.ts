bc1bcdb1070c61c1c55bc370d1a2c6d886779682
Starlin De La Cruz <starlinr82@gmail.com>
2024-01-07 01:54:45 -0400

    Fix: tabulation on main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
   .setVersion('1.0')
   .addTag('email')
   .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(3100);
 }
